b63cb53808433161d54c1d32df7f9a2e201d1ff8
Eden <edenw1@muskingum.edu>
2025-02-07 13:37:03 -0500

    updates from 2/7/25 complete restart from github

--- game_logic.py
+++ game_logic.py
@@ -1,16 +1,26 @@
-from game_object import GameObject
 from pubsub import pub
+from game_object import GameObject
+from player import Player
 
-class GameLogic:
+class GameWorld:
     def __init__(self):
         self.properties = {}
         self.game_objects = {}
+
         self.next_id = 0
 
-    def create_object(self, position, kind):
-        obj = GameObject(position, kind, self.next_id)
-        self.game_objects[obj.id] = obj
+    def create_object(self, position, kind, size):
+        # TODO: we'll need to create the right subclass here
+        # We need to work out how to know which one to use
+
+        if kind == "player":
+            obj = Player(position, kind, self.next_id, size)
+        else:
+            obj = GameObject(position, kind, self.next_id, size)
+
         self.next_id += 1
+        self.game_objects[obj.id] = obj
+
         pub.sendMessage('create', game_object=obj)
         return obj
 
@@ -18,12 +28,16 @@ class GameLogic:
         for id in self.game_objects:
             self.game_objects[id].tick()
 
+        # TODO: let the physics world get a tick in
+
     def load_world(self):
-        self.create_object([0, 0, 0], "cube")
+        self.create_object([0, 0, 0], "crate", (5,2,1))
+        self.create_object([0, -20, 0], "player", (1,1,1))
 
     def get_property(self, key):
         if key in self.properties:
             return self.properties[key]
+
         return None
 
     def set_property(self, key, value):
